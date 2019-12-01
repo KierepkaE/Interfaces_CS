85c26cd4184adba0ccccc933cddd5a823c951e23
kierepkae <kierepkaedyta@gmail.com>
2019-12-01 17:51:15 +0000

    refactor

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 namespace interfaces {
     class Program {
         static void Main (string[] args) {
-            var orderProcessor = new OrderProcessor ();
+            var orderProcessor = new OrderProcessor (IShippingCalculator ());
             var order = new Order { DatePlaced = DateTime.Now, TotalPrice = 100f };
             orderProcessor.Process (order);
         }
